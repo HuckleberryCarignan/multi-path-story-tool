fd940ea6adb1e9ce35e5a69c54e77faa78b534c2
Huckleberry Carignan <huckleberrycarignan@gmail.com>
2026-08-22 20:40:27 -0400

    Initial Commit

---
+++ multi-path-story-tool/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  multi-path-story-tool
+//
+//  Created by Christopher Carignan on 8/22/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
